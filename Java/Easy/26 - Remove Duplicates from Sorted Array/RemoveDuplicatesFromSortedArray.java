39ec4075f6fc9ee8f8e92d8438ddaf5f0687c3b7
David Jiang <djiang86@binghamton.edu>
2021-01-13 05:12:17 -0500

    remove duplicates from sorted array

---
+++ Java/Easy/26 - Remove Duplicates from Sorted Array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,15 @@
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+
+        for (int j = 0; j < nums.length; j++) {
+            nums[i++] = nums[j];
+
+            while (j != nums.length - 1 && nums[j] == nums[j + 1]) {
+                j++;
+            }
+        }
+
+        return i;
+    }
+}
