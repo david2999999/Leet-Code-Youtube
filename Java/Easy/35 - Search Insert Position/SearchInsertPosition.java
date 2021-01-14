ec894b12113da3568809cf5ffca5be4780f55d97
David Jiang <djiang86@binghamton.edu>
2021-01-14 05:12:00 -0500

    search insert position

---
+++ Java/Easy/35 - Search Insert Position/SearchInsertPosition.java
@@ -0,0 +1,19 @@
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return (nums[left] < target) ? left + 1 : left;
+    }
+}
