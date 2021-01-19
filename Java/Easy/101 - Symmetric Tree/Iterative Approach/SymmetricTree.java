8362f2e853a56ba7a072f2ce46272b519d8ff1ea
David Jiang <djiang86@binghamton.edu>
2021-01-19 05:15:15 -0500

    symmetric tree, symmetric approach

---
+++ Java/Easy/101 - Symmetric Tree/Iterative Approach/SymmetricTree.java
@@ -0,0 +1,21 @@
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode p = queue.poll(), q = queue.poll();
+
+            if (p == null && q == null) continue;
+            if ((p == null || q == null) || (p.val != q.val)) return false;
+
+            queue.add(p.left); queue.add(q.right);
+            queue.add(p.right); queue.add(q.left);
+        }
+
+        return true;
+    }
+}
