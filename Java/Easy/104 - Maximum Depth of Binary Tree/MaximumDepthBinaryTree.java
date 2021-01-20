5f2b95279cc1a9ab80a6d0dd97d8e1eca9ad8b88
David Jiang <djiang86@binghamton.edu>
2021-01-20 05:39:57 -0500

    maximum depth binary tree

---
+++ Java/Easy/104 - Maximum Depth of Binary Tree/MaximumDepthBinaryTree.java
@@ -0,0 +1,7 @@
+public class MaximumDepthBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
