02b191eef2a82d68355f049bd82df566ad956cac
David Jiang <djiang86@binghamton.edu>
2021-02-01 04:34:28 -0500

    search in rotated sorted array

---
+++ Java/Google/33 - Search in Rotated Sorted Array/SearchInRotatedSortedArray.java
@@ -0,0 +1,27 @@
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target >= nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
