1da413e6cbef3ee45409f0c920bf3aaff931a44d
David Jiang <djiang86@binghamton.edu>
2021-02-01 04:18:30 -0500

    remove element

---
+++ Java/Google/27 - Remove Element/RemoveElement.java
@@ -0,0 +1,13 @@
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i++] = nums[j];
+            }
+        }
+
+        return i;
+    }
+}
