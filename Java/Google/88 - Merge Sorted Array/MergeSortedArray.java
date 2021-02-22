27fc181c9cad892e96d6de9353bdc409123118de
David Jiang <djiang86@binghamton.edu>
2021-02-22 03:49:35 -0500

    merge sorted array

---
+++ Java/Google/88 - Merge Sorted Array/MergeSortedArray.java
@@ -0,0 +1,13 @@
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+
+        while (i >= 0 || j >= 0) {
+            if (j < 0 || (i >= 0 && nums1[i] >= nums2[j])) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+}
