8fe2a2210cf8fcbe9a15ea9d50b9c6f0021b04e1
David Jiang <djiang86@binghamton.edu>
2021-03-22 04:57:59 -0400

    best time to buy and sell stock III bottom up 2D matrix

--- Java/Google/123 - Best Time to Buy and Sell Stock III/Bottom Up 2D Matrix Approach/BestTimeToBuyAndSellStockIIIBottomUp2DApproach.java
+++ Java/Google/123 - Best Time to Buy and Sell Stock III/Bottom Up 2D Matrix Approach/BestTimeToBuyAndSellStockIIIBottomUp2DApproach.java
@@ -1,21 +1,21 @@
 public class BestTimeToBuyAndSellStockIIIBottomUp2DApproach {
     public int maxProfit(int[] prices) {
-        int[][][] maxProfit = new int[prices.length + 1][2][3];
+        int[][] maxProfit = new int[2][3];
 
         for (int i = prices.length - 1; i >= 0; i--) {
             for (int state = 1; state >= 0; state--) {
                 for (int t = 1; t <= 2; t++) {
                     if (state == 0) {
-                        maxProfit[i][state][t] =
-                            Math.max(maxProfit[i + 1][1][t] - prices[i], maxProfit[i + 1][state][t]);
+                        maxProfit[state][t] =
+                            Math.max(maxProfit[1][t] - prices[i], maxProfit[state][t]);
                     } else {
-                        maxProfit[i][state][t] =
-                            Math.max(maxProfit[i + 1][0][t - 1] + prices[i], maxProfit[i + 1][state][t]);
+                        maxProfit[state][t] =
+                            Math.max(maxProfit[0][t - 1] + prices[i], maxProfit[state][t]);
                     }
                 }
             }
         }
 
-        return maxProfit[0][0][2];
+        return maxProfit[0][2];
     }
 }
