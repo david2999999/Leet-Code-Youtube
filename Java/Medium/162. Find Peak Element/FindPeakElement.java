3f24880962285a14b3e72f6ad23f5fcc82a52ac6
David Jiang <djiang86@binghamton.edu>
2021-05-21 05:26:57 -0400

    find peak element

---
+++ Java/Medium/162. Find Peak Element/FindPeakElement.java
@@ -0,0 +1,17 @@
+public class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+}
