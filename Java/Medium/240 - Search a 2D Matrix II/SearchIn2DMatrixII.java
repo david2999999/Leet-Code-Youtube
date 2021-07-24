4062d06ba689002c02325f6dfbfae92d1a00778f
David Jiang <djiang86@binghamton.edu>
2021-07-24 04:45:30 -0400

    search a 2D matrix II

---
+++ Java/Medium/240 - Search a 2D Matrix II/SearchIn2DMatrixII.java
@@ -0,0 +1,17 @@
+public class SearchIn2DMatrixII {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r = 0, c = matrix[0].length - 1;
+
+        while (r < matrix.length && c >= 0) {
+            if (matrix[r][c] == target) {
+                return true;
+            } else if (matrix[r][c] < target) {
+                r = r + 1;
+            } else {
+                c = c - 1;
+            }
+        }
+
+        return false;
+    }
+}
